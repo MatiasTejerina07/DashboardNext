8c762b1041e0402392a984e7c0385d88be6ca071
Mat <matiastejerina94@gmail.com>
2023-12-20 09:14:30 -0300

    interface

--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -1,12 +1,21 @@
 'use client'
 
-
+import { useAppSelector } from "@/store"
 import { SimpleWidget } from "./SimpleWidget"
+import { IoCartOutline } from "react-icons/io5";
 
 export const WidgetsGrid = () => {
+
+    const counter = useAppSelector(store => store.counter.count)
+
     return (
         <div className="flex flex-wrap p-2">
-            <SimpleWidget />
+            <SimpleWidget
+                title="Contador"
+                label={counter.toString()}
+                icon={<IoCartOutline size={70} className='text-blue-400' />}
+                subTitle='Productos agregados'
+            />
         </div>
     )
 }
